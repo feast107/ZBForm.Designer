c11f701931a5e907f8d24f041f716016a716bd10
Feast <feast107@gmail.com>
2023-06-07 16:41:18 +0800

    Create config.js

---
+++ src/src/models/config.js
@@ -0,0 +1,11 @@
+import { TemplateItem } from "./template";
+
+export class ConfigItem{
+    /**
+     * 实例化一个模板
+     * @param {TemplateItem} templateItem 
+     */
+    constructor(templateItem){
+        this.template = templateItem;
+    }
+}
