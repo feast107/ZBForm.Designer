043b1b24e0f8845ad58d7f5ce27f8ac28c420247
Feast <feast107@gmail.com>
2023-06-08 10:02:03 +0800

    Update DesignView.vue

--- src/src/components/DesignView.vue
+++ src/src/components/DesignView.vue
@@ -3,6 +3,9 @@
         <div id="Container" class="fill" style="z-index: 50;top:0; left :0; position: absolute;">
             <DragableRect style="background-color: #00f3f380;" :rect="rect">
             </DragableRect>
+            <DragableTable style="background-color: #33f3a380;" :rect="table"
+            :row-definitions="[10,10]" :col-definitions="[10,20]"
+            ></DragableTable>
         </div>
         <div id="Background" class="fill" style="top:0; position: absolute;">
             <img class="fill" src="../assets/background.png" />
@@ -15,15 +18,18 @@
 <script>
 import { Rect } from '@/utils/drawing/rect'
 import DragableRect from './controls/DragableRect.vue';
+import DragableTable from './controls/DragableTable.vue';
 export default {
     components: {
-        DragableRect
+        DragableRect,
+        DragableTable
     },
     data() {
         window.Rect = Rect;
         return {
             drawer: false,
-            rect: new Rect(50, 50, 50, 50)
+            rect: new Rect(50, 50, 50, 50),
+            table: new Rect(200,200,100,100)
         };
     },
     methods: {
