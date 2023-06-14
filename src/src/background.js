9c29fa4821a35027b438e8b006f6dd3a7df16dcb
Feast <feast107@gmail.com>
2023-06-14 11:12:36 +0800

    Update background.js

--- src/src/background.js
+++ src/src/background.js
@@ -1,8 +1,9 @@
 "use strict";
 
-import { app, protocol, BrowserWindow } from "electron";
+import { app, protocol, session, BrowserWindow } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS3_DEVTOOLS } from "electron-devtools-installer";
+
 import path from 'path'
 const isDevelopment = process.env.NODE_ENV !== "production";
 
@@ -61,6 +62,7 @@ app.on("activate", () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", async () => {
+    require('vue-devtools').install();
     if (isDevelopment && !process.env.IS_TEST) {
         // Install Vue Devtools
         try {
@@ -69,7 +71,8 @@ app.on("ready", async () => {
             console.error("Vue Devtools failed to install:", e.toString());
         }
     }
-    createWindow();
+
+    await createWindow();
 });
 
 // Exit cleanly on request from parent process in development mode.
