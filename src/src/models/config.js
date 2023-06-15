92c3eee09ceaf99993b9c05f7e9f6a72842a0aee
Feast <feast107@gmail.com>
2023-06-15 10:00:41 +0800

    Update config.js

--- src/src/models/config.js
+++ src/src/models/config.js
@@ -1,11 +1,38 @@
-import { TemplateItem } from "./template";
+import {TemplateItem} from "./template";
+import {Unit} from "@/models/unit";
+import {Table} from "@/models/table";
 
-export class ConfigItem{
+export class UnitConfig {
     /**
-     * 实例化一个模板
-     * @param {TemplateItem} templateItem 
+     * @param other
      */
-    constructor(templateItem){
-        this.template = templateItem;
+    constructor(other) {
+        this.type = 'unit';
+        this.mode = other ? other.mode : '';
+        this.region = new Unit(other ? other.region : null);
+    }
+
+    get copy(){
+        return new UnitConfig(this);
+    }
+}
+
+export class TableConfig {
+    constructor(other) {
+        this.type = 'table';
+        this.modes = other != null
+            ? {
+                direction: other.modes.direction,
+                configs: other.modes.configs
+            }
+            : {
+                direction: 'horizontal',
+                configs: [],
+            }
+        this.region = new Table(other != null ? other.region : null);
+    }
+
+    get copy() {
+        return new TableConfig(this);
     }
 }
