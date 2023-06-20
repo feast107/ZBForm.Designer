0f9550f861adfcd4ac5daca87f58d42e52338d4d
Feast <feast107@gmail.com>
2023-06-20 15:08:00 +0800

    Update template.js

--- src/src/models/template.js
+++ src/src/models/template.js
@@ -1,11 +1,24 @@
+import {Config} from "@/models/config";
+
 export class TemplateGroup{
-    constructor(){
-        this.templateItems = [];
+    constructor(label,icon,style,items) {
+        this.label = label;
+        this.icon = icon;
+        this.style = style;
+        this.items = items;
+        items.forEach(x=>{
+            x.style = style;
+        })
     }
 }
 
-export class TemplateItem{
-    constructor(){
-        this.name = String();
+
+export class TemplateItem {
+    constructor(label, type, color, event, options) {
+        this.label = label;
+        this.type = type;
+        this.color = Config.modifyColor(color)
+        this.event = event;
+        this.options = options;
     }
 }
