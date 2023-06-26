c8a54286e8d9718a606421ff9191c18b77bf5f5c
Feast <feast107@gmail.com>
2023-06-26 15:41:00 +0800

    Create form.js

---
+++ src/src/models/form.js
@@ -0,0 +1,9 @@
+export class Form{
+    constructor() {
+        this.formId = null;
+        this.formUrl = null;
+        this.pageHeight = null;
+        this.pageWidth = null;
+        this.pageCount = null;
+    }
+}
