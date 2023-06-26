1e69c8b86c34a1159c1498c7e60524d78901553e
Feast <feast107@gmail.com>
2023-06-26 10:12:32 +0800

    Create location.js

---
+++ src/src/configs/location.js
@@ -0,0 +1,27 @@
+const concat = (a,b) => `${a}/${b}`
+
+export class Url {
+    static get Domain() {
+        return "http://47.93.86.37:8383";
+    }
+
+    static get authservice() {
+        let route = "authservice";
+        return {
+            route ,
+            url: concat('',route),
+            get admin() {
+                let url = this.url;
+                let route = 'admin';
+                return {
+                    route,
+                    url: concat(url,route),
+                    get queryItem() {
+                        let url = this.url
+                        return concat(url,'queryItem');
+                    }
+                };
+            }
+        };
+    }
+}
